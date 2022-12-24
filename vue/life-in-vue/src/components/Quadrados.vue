42d7b6998baf3be7c941bf2f84d6f6acb6db52a6
Gabriela Victoria <gabrielavictoria91@gmail.com>
2022-12-23 23:55:15 -0300

    resulvido bug que n morre

--- vue/life-in-vue/src/components/Quadrados.vue
+++ vue/life-in-vue/src/components/Quadrados.vue
@@ -4,47 +4,47 @@
     <hr style="margin-top: 0px; color: #d3d6da" />
     <div class="container-tabuleiro">
         <div class="tabuleiro">
-            <div id="l1c1" class="purple"></div>
-            <div id="l1c2" class="purple"></div>
-            <div id="l1c3" class="purple"></div>
-            <div id="l1c4" class="purple"></div>
-            <div id="l1c5" class="purple"></div>
+            <div id="l1c1" class=" todos quadrado"></div>
+            <div id="l1c2" class=" todos purple"></div>
+            <div id="l1c3" class=" todos purple"></div>
+            <div id="l1c4" class=" todos purple"></div>
+            <div id="l1c5" class=" todos quadrado"></div>
         </div>
 
         <div class="tabuleiro">
-            <div id="l2c1" class="quadrado"></div>
-            <div id="l2c2" class="quadrado"></div>
-            <div id="l2c3" class="purple"></div>
-            <div id="l2c4" class="quadrado"></div>
-            <div id="l2c5" class="quadrado"></div>
+            <div id="l2c1" class=" todos quadrado"></div>
+            <div id="l2c2" class=" todos quadrado"></div>
+            <div id="l2c3" class=" todos quadrado"></div>
+            <div id="l2c4" class=" todos quadrado"></div>
+            <div id="l2c5" class=" todos quadrado"></div>
         </div>
         <div class="tabuleiro">
-            <div id="l3c1" class="quadrado"></div>
-            <div id="l3c2" class="quadrado"></div>
-            <div id="l3c3" class="purple"></div>
-            <div id="l3c4" class="quadrado"></div>
-            <div id="l3c5" class="quadrado"></div>
+            <div id="l3c1" class=" todos quadrado"></div>
+            <div id="l3c2" class=" todos quadrado"></div>
+            <div id="l3c3" class=" todos quadrado"></div>
+            <div id="l3c4" class=" todos quadrado"></div>
+            <div id="l3c5" class=" todos quadrado"></div>
         </div>
         <div class="tabuleiro">
-            <div id="l4c1" class="quadrado"></div>
-            <div id="l4c2" class="quadrado"></div>
-            <div id="l4c3" class="purple"></div>
-            <div id="l4c4" class="quadrado"></div>
-            <div id="l4c5" class="quadrado"></div>
+            <div id="l4c1" class=" todos quadrado"></div>
+            <div id="l4c2" class=" todos quadrado"></div>
+            <div id="l4c3" class=" todos quadrado"></div>
+            <div id="l4c4" class=" todos quadrado"></div>
+            <div id="l4c5" class=" todos quadrado"></div>
         </div>
         <div class="tabuleiro">
-            <div id="l5c1" class="quadrado"></div>
-            <div id="l5c2" class="quadrado"></div>
-            <div id="l5c3" class="quadrado"></div>
-            <div id="l5c4" class="quadrado"></div>
-            <div id="l5c5" class="quadrado"></div>
+            <div id="l5c1" class=" todos quadrado"></div>
+            <div id="l5c2" class=" todos quadrado"></div>
+            <div id="l5c3" class=" todos quadrado"></div>
+            <div id="l5c4" class=" todos quadrado"></div>
+            <div id="l5c5" class=" todos quadrado"></div>
         </div>
         <div class="tabuleiro">
-            <div id="l6c1" class="quadrado"></div>
-            <div id="l6c2" class="quadrado"></div>
-            <div id="l6c3" class="quadrado"></div>
-            <div id="l6c4" class="quadrado"></div>
-            <div id="l6c5" class="quadrado"></div>
+            <div id="l6c1" class=" todos quadrado"></div>
+            <div id="l6c2" class=" todos quadrado"></div>
+            <div id="l6c3" class=" todos quadrado"></div>
+            <div id="l6c4" class=" todos quadrado"></div>
+            <div id="l6c5" class=" todos quadrado"></div>
         </div>
     </div>
     <button @click="test()"> Play </button>
@@ -56,15 +56,16 @@ export default {
   name: 'QuadradosLife',
   methods: {
     test() {
-// Pega estádo inicial antes do play
-      let myArray = new Array()
-      for (var ls = 1; ls <= 6; ls++){
-        for(var cs = 1; cs <= 5; cs++){ 
-      let pega = document.getElementById(`l${l}c${c}`)
-      myArray.push(pega)
-        }
-      }
-      console.log("MOSTRAAAAA", myArray)
+    // Pega estádo inicial antes do play
+    // let pega = document.getElementsByClassName('todos');
+      // let myArray = new Array()
+      // for (var ls = 1; ls <= 6; ls++){
+      //   for(var cs = 1; cs <= 5; cs++){ 
+      // let pega = document.getElementById(`l${l}c${c}`)
+      // myArray.push(pega)
+      //   }
+      // }
+      // console.log("MOSTRAAAAA", myArray)
 
 
       let clas = document.getElementsByClassName('purple');
@@ -105,18 +106,17 @@ export default {
           }
 
           if (quanVizi < 2 || quanVizi < 3) {
-            console.log(idTemp, "morre")
-            // document.getElementById(idTemp).classList.add("quadrado")
+            if(document.getElementById(idTemp).classList == "todos purple" ){
+              document.getElementById(idTemp).classList.replace("purple", "quadrado")
+            }else{
+              document.getElementById(idTemp).classList.add("quadrado")
+            }
           }else if (1 < quanVizi <= 3 ){
-            console.log(idTemp, "vivi")
-            // document.getElementById(idTemp).classList.add("purple")
+            // console.log(idTemp, "vivi")
+            document.getElementById(idTemp).classList.add("purple")
           }             
-          console.log(idTemp, quanVizi)
+          // console.log(idTemp, quanVizi)
         }
-
-
-      //     
-      //     console.log(idTemp)
       }
     }
   }
