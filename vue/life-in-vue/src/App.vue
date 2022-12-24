e8c23fd81e0368cc81c0c41c6105b49e17ab8dd1
Gabriela Victoria <gabrielavictoria91@gmail.com>
2022-12-23 23:14:30 -0300

    sei quem morre e vii

--- vue/life-in-vue/src/App.vue
+++ vue/life-in-vue/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <QuadradosLife msg="Welcome to Your Vue.js App"/>
+    <QuadradosLife/>
   </div>
 </template>
 
--- vue/life-in-vue/src/components/Quadrados.vue
+++ vue/life-in-vue/src/components/Quadrados.vue
@@ -4,31 +4,31 @@
     <hr style="margin-top: 0px; color: #d3d6da" />
     <div class="container-tabuleiro">
         <div class="tabuleiro">
-            <div id="l1c1" class="quadrado"></div>
-            <div id="l1c2" class="quadrado"></div>
-            <div id="l1c3" class="quadrado"></div>
-            <div id="l1c4" class="quadrado"></div>
-            <div id="l1c5" class="quadrado"></div>
+            <div id="l1c1" class="purple"></div>
+            <div id="l1c2" class="purple"></div>
+            <div id="l1c3" class="purple"></div>
+            <div id="l1c4" class="purple"></div>
+            <div id="l1c5" class="purple"></div>
         </div>
 
         <div class="tabuleiro">
             <div id="l2c1" class="quadrado"></div>
             <div id="l2c2" class="quadrado"></div>
-            <div id="l2c3" class="quadrado"></div>
+            <div id="l2c3" class="purple"></div>
             <div id="l2c4" class="quadrado"></div>
             <div id="l2c5" class="quadrado"></div>
         </div>
         <div class="tabuleiro">
             <div id="l3c1" class="quadrado"></div>
             <div id="l3c2" class="quadrado"></div>
-            <div id="l3c3" class="quadrado"></div>
+            <div id="l3c3" class="purple"></div>
             <div id="l3c4" class="quadrado"></div>
             <div id="l3c5" class="quadrado"></div>
         </div>
         <div class="tabuleiro">
             <div id="l4c1" class="quadrado"></div>
             <div id="l4c2" class="quadrado"></div>
-            <div id="l4c3" class="quadrado"></div>
+            <div id="l4c3" class="purple"></div>
             <div id="l4c4" class="quadrado"></div>
             <div id="l4c5" class="quadrado"></div>
         </div>
@@ -47,14 +47,78 @@
             <div id="l6c5" class="quadrado"></div>
         </div>
     </div>
+    <button @click="test()"> Play </button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'QuadradosLife',
-  props: {
-    msg: String
+  methods: {
+    test() {
+// Pega estádo inicial antes do play
+      let myArray = new Array()
+      for (var ls = 1; ls <= 6; ls++){
+        for(var cs = 1; cs <= 5; cs++){ 
+      let pega = document.getElementById(`l${l}c${c}`)
+      myArray.push(pega)
+        }
+      }
+      console.log("MOSTRAAAAA", myArray)
+
+
+      let clas = document.getElementsByClassName('purple');
+      for (var l = 1; l <= 6; l++){
+        for(var c = 1; c <= 5; c++){
+          let idTemp = `l${l}c${c}`
+          let quanVizi = 0 
+          if (clas.namedItem(`l${l-1}c${c-1}`)){
+            quanVizi++; 
+          }
+          
+          if (clas.namedItem(`l${l-1}c${c}`)){
+            quanVizi++ 
+          }
+          
+          if (clas.namedItem(`l${l-1}c${c+1}`)){
+            quanVizi++ 
+          }
+          
+          if (clas.namedItem(`l${l}c${c+1}`)){
+            quanVizi++ 
+          }
+          
+          if (clas.namedItem(`l${l}c${c-1}`)){
+            quanVizi++ 
+          }
+          
+          if (clas.namedItem(`l${l+1}c${c-1}`)){
+            quanVizi++ 
+          }
+          
+          if (clas.namedItem(`l${l+1}c${c}`)){
+            quanVizi++ 
+          }
+          
+          if (clas.namedItem(`l${l+1}c${c+1}`)){
+            quanVizi++ 
+          }
+
+          if (quanVizi < 2 || quanVizi < 3) {
+            console.log(idTemp, "morre")
+            // document.getElementById(idTemp).classList.add("quadrado")
+          }else if (1 < quanVizi <= 3 ){
+            console.log(idTemp, "vivi")
+            // document.getElementById(idTemp).classList.add("purple")
+          }             
+          console.log(idTemp, quanVizi)
+        }
+
+
+      //     
+      //     console.log(idTemp)
+      }
+    }
   }
 }
 </script>
